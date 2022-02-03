e0ae6ba36f39624a2920fa7e9752d26c3f9589d0
Sankalp Saini <sankalpsaini6@gmail.com>
2022-02-02 18:05:23 -0700

    added a color

--- app/src/main/java/com/example/qrgo/Triangle.java
+++ app/src/main/java/com/example/qrgo/Triangle.java
@@ -2,6 +2,7 @@ package com.example.qrgo;
 
 public class Triangle extends Shape {
     private int x, y;
+    String color = "Blue";
     public void Shape(int size) {
         x = size;
         y = size;
