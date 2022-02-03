b7e6876f15a3667b0a29dcb1ce16beecccc26b92
amanudde1999 <amanudde@ualberta.ca>
2022-02-02 17:12:21 -0700

    Revert "Added color attribute to shape class."
    This reverts commit 8531d7eb

--- app/src/main/java/com/example/qrgo/Shape.java
+++ app/src/main/java/com/example/qrgo/Shape.java
@@ -1,10 +1,8 @@
 package com.example.qrgo;
 
 public abstract class Shape {
-    protected int x;
-    protected int y;
-
-    private String color = "blue";
+    private int x;
+    private int y;
 }
 
 
