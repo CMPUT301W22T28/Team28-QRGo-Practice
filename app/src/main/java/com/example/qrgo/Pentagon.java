c0d2a2e8faa6a183a59b491a57d00423a1e668d4
Ervin Binu Joseph <ervinbinujoseph@gmail.com>
2022-02-03 17:09:19 -0700

    Added Pentagon Shape

--- app/src/main/java/com/example/qrgo/Pentagon.java
+++ app/src/main/java/com/example/qrgo/Pentagon.java
@@ -1,2 +1,10 @@
-package com.example.qrgo;public class Pentagon {
+package com.example.qrgo;
+
+public class Pentagon extends Shape{
+    protected int x,y;
+
+    public Pentagon(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
